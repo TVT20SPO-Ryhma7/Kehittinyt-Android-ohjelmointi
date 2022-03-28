4b8d826f76e1d2ed0b06ff9b8a1b56b344141579
Aleksi Kalliokoski <a.kalliokoski@hotmail.com>
2022-03-28 22:09:21 +0300

    First version of the demo

--- app/src/main/java/com/example/roomsqlite/MainActivity.kt
+++ app/src/main/java/com/example/roomsqlite/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.roomsqlite
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.room.Room
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = Room.databaseBuilder(
+            applicationContext,
+            UserDatabase::class.java, "TestDB"
+        ).build()
+
+        val userDao = db.userDao()
+        val users: List<User> = userDao.getAllUsers()
     }
 }
---
+++ app/src/main/java/com/example/roomsqlite/UserDao.kt
@@ -0,0 +1,27 @@
+package com.example.roomsqlite
+
+import androidx.room.*
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM user")
+    fun getAllUsers(): List<User>
+
+    @Query("SELECT * FROM user WHERE firstName LIKE :firstName")
+    fun getUsersByName(firstName: String): List<User>
+
+    @Insert
+    fun addUser(user: User)
+
+    @Update
+    fun updateUser(user: User)
+
+    @Delete
+    fun deleteUser(user: User)
+
+
+
+    // Esimerkki käännösaikaisesta kyselyn tarkistuksesta:
+    @Query("SELECT * FROM userss")
+    fun getAll(): List<User>
+}
